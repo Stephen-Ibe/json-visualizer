25051a96a9fc4d5de084f8585a0f2b4f29a8faa1
Stephen Ibe <stephen.ibe@fairmoney.io>
2025-08-25 11:01:19 +0100

    integrated tanstack query

--- src/components/providers/AppProvider.tsx
+++ src/components/providers/AppProvider.tsx
@@ -1,9 +1,26 @@
 import { lightTheme } from "@/lib";
-import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React, { useState } from "react";
 import { ThemeProvider } from "styled-components";
 
 const AppProvider = ({ children }: { children: React.ReactNode }) => {
-  return <ThemeProvider theme={lightTheme}>{children}</ThemeProvider>;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 5,
+            retry: false,
+          },
+        },
+      })
+  );
+
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </ThemeProvider>
+  );
 };
 
 export default AppProvider;
